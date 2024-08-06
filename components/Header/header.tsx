cc357a0e4d38b1696e1ed5da5f38a13712fa0585
HerveKilanda <kilandaherve@gmail.com>
2024-08-06 11:31:25 +0200

    addSearchButton

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -7,6 +7,8 @@ import { getMe } from "@/utils/get-me";
 import { searchManga } from "@/utils/search-manga";
 import { useRouter } from "next/navigation";
 
+import { FaSearch } from "react-icons/fa";
+
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [user, setUser] = useState(null);
@@ -35,7 +37,11 @@ export default function Header() {
       const data = await searchManga(searchQuery);
       console.log(data);
       router.push(
-        `/search-results/${encodeURIComponent(searchQuery)}?query=${encodeURIComponent(searchQuery)}&results=${encodeURIComponent(JSON.stringify(data))}`
+        `/search-results/${encodeURIComponent(
+          searchQuery
+        )}?query=${encodeURIComponent(
+          searchQuery
+        )}&results=${encodeURIComponent(JSON.stringify(data))}`
       );
     } catch (error) {
       console.error("Error during search:", error);
@@ -46,14 +52,25 @@ export default function Header() {
     <nav className="flex flex-wrap items-center justify-between p-4 bg-blue-700 text-white shadow-md">
       <div className="flex items-center justify-between flex-grow sm:flex-grow-0">
         <h1 className="text-3xl">Otakulinks</h1>
-        <button onClick={() => setIsOpen(!isOpen)} className="sm:hidden text-3xl">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="sm:hidden text-3xl"
+        >
           <i>Menu</i>
         </button>
       </div>
-      <div className={`${isOpen ? 'block' : 'hidden'} sm:block flex-grow sm:flex sm:items-center sm:justify-center`}>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } sm:block flex-grow sm:flex sm:items-center sm:justify-center`}
+      >
         <SearchInput />
       </div>
-      <div className={`${isOpen ? 'block' : 'hidden'} flex sm:flex-grow sm:flex sm:items-center sm:space-x-4 sm:justify-end text-xl gap-4 mt-4 sm:mt-0`}>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } flex sm:flex-grow sm:flex sm:items-center sm:space-x-4 sm:justify-end text-xl gap-4 mt-4 sm:mt-0`}
+      >
         <NavLink href="/" text="Home" />
         <NavLink href="/inscription" text="Inscription" />
         <NavLink href="/connexion" text="Connexion" />
@@ -65,22 +82,27 @@ export default function Header() {
   function SearchInput() {
     return (
       <div className="relative">
-        <form onSubmit={handleSearchSubmit}>
-          <input
-            type="search"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="w-96 sm:w-96 h-12 px-4 rounded-full text-black border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
-            placeholder="Rechercher un manga"
-          />
-        </form>
+        <input
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          type="search"
+          className="w-96 sm:w-96 h-12 px-4 rounded-full text-black border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
+          placeholder="Rechercher un manga"
+          autoFocus
+        />
+         <button onClick={handleSearchSubmit} type="submit" className="ml-2 p-2 rounded-full bg-blue-500 hover:bg-blue-600 text-white">
+          <FaSearch  />
+        </button>
       </div>
     );
   }
 
   function NavLink({ href, text }) {
     return (
-      <Link href={href} className="transition-colors duration-300 hover:text-gray-300">
+      <Link
+        href={href}
+        className="transition-colors duration-300 hover:text-gray-300"
+      >
         {text}
       </Link>
     );
